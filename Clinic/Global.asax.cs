c7bd7d978cd4cbdbc5b6e6738bbf0b8a3d006e91
Unknown <slawomir.lyskawinski@gmail.com>
2018-11-10 21:05:51 +0100

    Dodanie autofaca
    +build containera

--- Clinic/Global.asax.cs
+++ Clinic/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Autofac;
+using Autofac.Integration.Mvc;
 
 namespace Clinic
 {
@@ -12,10 +14,20 @@ namespace Clinic
 	{
 		protected void Application_Start()
 		{
+			ConfigureContainer();
 			AreaRegistration.RegisterAllAreas();
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
 			BundleConfig.RegisterBundles(BundleTable.Bundles);
+		}
+
+		private void ConfigureContainer()
+		{
+			var builder = new ContainerBuilder();
+			builder.RegisterControllers(typeof(MvcApplication).Assembly);
+			builder.RegisterAssemblyModules(typeof(MvcApplication).Assembly);
+			var container = builder.Build();
+			DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
 		}
 	}
 }
