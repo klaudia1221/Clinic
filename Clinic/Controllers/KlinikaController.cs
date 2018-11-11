5733075ab9c15c69bd3db6ccbd059ba49febbdc3
Unknown <slawomir.lyskawinski@gmail.com>
2018-11-11 15:18:03 +0100

    Edycje w kontrolerach
    Dostosowanie kontrolerów na potrzeby projektu. Dodanie widoku doktora i pacjenta

---
+++ Clinic/Controllers/KlinikaController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+
+namespace Clinic.Controllers
+{
+	public class KlinikaController : Controller
+	{
+		public ActionResult Index()
+		{
+			ViewBag.Title = "System zarządzania kliniką";
+
+			return View();
+		}
+		public ActionResult Doktor()
+		{
+			ViewBag.Title = "Doktorzy";
+
+			return View();
+		}
+
+		public ActionResult Pacjent()
+		{
+			ViewBag.Title = "Pacjenci";
+
+			return View();
+		}
+	}
+}
