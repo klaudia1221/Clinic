9fa4d151d6aa95e92972b7fa4d4d2a71f3011f63
klaudia1221 <klaudia.b1221@gmail.com>
2019-01-05 23:18:25 +0100

    Prawie działa

---
+++ Clinic/Controllers/ReceptaController.cs
@@ -0,0 +1,149 @@
+﻿using Clinic.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Clinic.Controllers
+{
+    public class ReceptaController : Controller
+    {
+
+        public ReceptaController(KlinikaEntities db)
+        {
+            this.db = db;
+            ViewBag.Title = Title;
+        }
+        // GET: Recepta
+
+        public ActionResult Index(string message)
+        {
+            var prescriptions = db.Recepta;
+
+            ViewBag.ErrorMessage = message;
+
+            return View(prescriptions.ToList());
+        }
+
+        public ActionResult Nowy()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Nowy(
+            [Bind(Include = "PacjentID, DoktorID, Data, Lek, Refundacja, Status")]
+            Recepta prescription)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Recepta.Add(prescription);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DbUpdateException ex)
+            {
+                var errorMessage = $"Nie udało się dodać recepty - {ex.InnerException.InnerException.Message}";
+
+                ViewBag.ErrorMessage = errorMessage;
+            }
+
+            return View(prescription);
+        }
+        public ActionResult Edytuj(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var prescription = db.Recepta.Find(id);
+
+            if (prescription == null)
+                return HttpNotFound();
+
+            return View(prescription);
+        }
+
+        [HttpPost, ActionName("Edytuj")]
+        public ActionResult EdytujPost(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var prescriptionToUpdate = db.Recepta.Find(id);
+
+            if (TryUpdateModel(prescriptionToUpdate, "",
+                new[] { "PacjentID, DoktorID, Data, Lek, Refundacja, Status" }))
+            {
+                try
+                {
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+                catch (DbUpdateException ex)
+                {
+                    var errorMessage = $"Nie udało się edytować recepty - {ex.InnerException.InnerException.Message}";
+
+                    ViewBag.ErrorMessage = errorMessage;
+                }
+            }
+
+            return View(prescriptionToUpdate);
+        }
+
+        public ActionResult Szczegoly(int id)
+        {
+            var prescription = db.Recepta.Find(id);
+
+            if (prescription == null)
+                return HttpNotFound();
+
+            return View(prescription);
+        }
+
+        public ActionResult Usun(int? id, bool? saveChangesError = false)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            if (saveChangesError.GetValueOrDefault())
+                ViewBag.ErrorMessage = $"Nie udało się usunąć recepty o identyfikatorze: {id}";
+
+            var prescription = db.Recepta.Find(id);
+
+            if (prescription == null)
+                return HttpNotFound();
+
+            return View(prescription);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Usun(int id)
+        {
+            try
+            {
+                var prescription = db.Recepta.Find(id);
+                db.Recepta.Remove(prescription);
+                db.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Usun", new { id = id, saveChangesError = true });
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
+        private readonly KlinikaEntities db;
+        private const string Title = "Recepty";
+    }
+}
---
+++ Clinic/Controllers/ZaopatrzenieController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Data.Entity.Infrastructure;
+using System.Web.Mvc;
+using Clinic.Models;
+using System.Linq;
+using System.Net;
+
+namespace Clinic.Controllers
+{
+    public class ZaopatrzenieController : Controller
+    {
+
+        public ZaopatrzenieController(KlinikaEntities db)
+        {
+            this.db = db;
+            ViewBag.Title = Title;
+        }
+        // GET
+        public ActionResult Index()
+        {
+            var zaopatrzenie = db.Zaopatrzenie;
+
+            return View(zaopatrzenie.ToList());
+
+
+        }
+        public ActionResult Nowy()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Nowy(
+            [Bind(Include = "Nazwa,Opis,Cena,Dostepnosc")]
+            Zaopatrzenie zaopatrzenie)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+             
+                    db.Zaopatrzenie.Add(zaopatrzenie);
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DbUpdateException ex)
+            {
+                var errorMessage = $"Nie udało się dodać zaopatrzenia - {ex.InnerException.InnerException.Message}";
+
+                ViewBag.ErrorMessage = errorMessage;
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ErrorMessage = ex.Message;
+            }
+
+            return View(zaopatrzenie);
+        }
+        public ActionResult Edytuj(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var zaopatrzenie = db.Zaopatrzenie.Find(id);
+
+            if (zaopatrzenie == null)
+                return HttpNotFound();
+
+            return View(zaopatrzenie);
+        }
+
+        [HttpPost, ActionName("Edytuj")]
+        public ActionResult EdytujPost(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var zaopatrzenieToUpdate = db.Zaopatrzenie.Find(id);
+
+            TryUpdateModel(zaopatrzenieToUpdate, "",
+                new[] {"Nazwa, Opis, Cena, Dostepnosc"});
+            
+                db.SaveChanges();
+                return RedirectToAction("Index");
+                //try
+                //{
+                //    db.SaveChanges();
+
+                //    return RedirectToAction("Index");
+                //}
+                //catch (DbUpdateException ex)
+                //{
+                //    var errorMessage = $"Nie udało się edytować zaopatrzenia - {ex.InnerException.InnerException.Message}";
+
+                //    ViewBag.ErrorMessage = errorMessage;
+                //}
+            
+
+            return View(zaopatrzenieToUpdate);
+        }
+
+        public ActionResult Szczegoly(int id)
+        {
+            var zaopatrzenie = db.Zaopatrzenie.Find(id);
+
+            if (zaopatrzenie == null)
+                return HttpNotFound();
+
+            return View(zaopatrzenie);
+        }
+
+        public ActionResult Usun(int? id, bool? saveChangesError = false)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            if (saveChangesError.GetValueOrDefault())
+                ViewBag.ErrorMessage = $"Nie udało się usunąć zaopatrzenia o identyfikatorze: {id}";
+
+            var zaopatrzenie = db.Zaopatrzenie.Find(id);
+
+            if (zaopatrzenie == null)
+                return HttpNotFound();
+
+            return View(zaopatrzenie);
+        }
+
+        [HttpPost]
+        public ActionResult Usun(int id)
+        {
+            try
+            {
+                var zaopatrzenie = db.Zaopatrzenie.Find(id);
+                db.Zaopatrzenie.Remove(zaopatrzenie);
+                db.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Usun", new { id = id, saveChangesError = true });
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
+        private readonly KlinikaEntities db;
+        private const string Title = "Zaopatrzenie";
+    }
+}
